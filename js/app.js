49d7990e9c00dad3e4520d885d71a7ecc669bd98
Shaher-11 <shahershamroukh@gmail.com>
2020-12-05 18:59:38 +0200

    this the landing page for udacity program

---
+++ js/app.js
@@ -0,0 +1,110 @@
+//Build the menu dynamically
+
+let unorderedList = document.getElementById("unordered-list");
+let containerCount = document.getElementsByClassName("landing__container")
+  .length;
+
+for (i = 1; i < containerCount + 1; i++) {
+  let item = "#section" + i + "-title";
+  let itemValue = document.querySelector(item);
+  let itemText = itemValue.textContent;
+  let newLine = document.createElement("li");
+  let lineText = document.createTextNode(itemText);
+  let listItem = "sample-nav-" + i;
+  newLine.setAttribute("id", listItem);
+  newLine.setAttribute("class", "nav-list-item");
+  newLine.appendChild(lineText);
+  unorderedList.appendChild(newLine);
+
+  let itemTarget = document.getElementById("section" + i);
+  let listTarget = document.getElementById(listItem);
+  let buttonName = "section" + i + "-button";
+  let buttonToAdd = document.getElementById(buttonName);
+
+  //Adds button when scrolls into view
+
+  listTarget.addEventListener("click", function() {
+    itemTarget.scrollIntoView ({
+      behavior: 'smooth'}
+    )
+//Added smoth scrolling 
+    buttonToAdd.innerHTML =
+      "<button class='section-button' onclick='goToTop()'>Return to Top</button>";
+  });
+}
+
+
+
+
+//This is the  function for a scroll 
+const scrollToTop = () => {
+  const scrolling = document.documentElement.scrollTop || document.body.scrollTop;
+  if (scrolling > 0) {
+    window.requestAnimationFrame(scrollToTop);
+    window.scrollTo(0, scrolling - scrolling / 50);
+  }
+};
+
+
+// On button click, goes up to top of page 
+function goToTop() {
+scrollToTop();
+
+  for (i = 1; i < containerCount + 1; i++) {
+    let buttonToDelete = document.getElementById("section" + i + "-button");
+    buttonToDelete.innerHTML = "";
+  }
+}
+
+//Checks if section is in view and adds active-class with moving background and color change
+function checkIfSectionInView() {
+  let isInViewport = function(elem) {
+    let bounding = elem.getBoundingClientRect();
+    return (
+      bounding.top <= 50 &&
+      bounding.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <=
+        (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  for (i = 1; i < containerCount + 1; i++) {
+    let sectionInFullView = document.getElementById("section" + i);
+
+    window.addEventListener(
+      "scroll",
+      function(event) {
+        if (isInViewport(sectionInFullView)) {
+          sectionInFullView.classList.add("your-active-class");
+        } else {
+          sectionInFullView.classList.remove("your-active-class");
+        }
+      },
+      false
+    );
+  }
+}
+
+//Add sticky header as per review (Used tutorial here: https://www.w3schools.com/howto/howto_js_navbar_sticky.asp)
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("header");
+console.log(navbar)
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+console.log(sticky)
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+checkIfSectionInView();
---
+++ readme.md
@@ -0,0 +1,9 @@
+# Landing Page
+
+This project is for Udacity's  program. HTML and CSS starter code provided by Udacity. 
+
+## Description
+
+This is a basic landing page that uses Javascript to create a NavBar dynamically and to highlight sections when scrolled. The project uses HTML, CSS, as well as Javascript. 
+
+
